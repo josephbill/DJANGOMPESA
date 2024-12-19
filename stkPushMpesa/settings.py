1e573970968966a324c072d213fd78b277850971
josephbill <josephbill00@gmail.com>
2024-12-20 01:08:20 +0300

    added procfile and other resources for deployment

--- stkPushMpesa/settings.py
+++ stkPushMpesa/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from django.contrib import staticfiles
+
 from stkapp.utility import BASE_NGROK_URL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-fahx8-+w*dkqbf-y+wp4%vuxri-yitkjhp(ee6884whefe2ygh"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['ee55-197-237-204-60.ngrok-free.app','127.0.0.1']
+ALLOWED_HOSTS = ['ee55-197-237-204-60.ngrok-free.app','127.0.0.1','stkPushMpesa.onrender.com']
 
 
 # Application definition
@@ -132,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
