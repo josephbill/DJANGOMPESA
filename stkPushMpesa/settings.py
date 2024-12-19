7c9b06053b5404a50470a620ebdf7440af70b39b
josephbill <josephbill00@gmail.com>
2024-12-20 01:29:22 +0300

    fixing wsgi error

--- stkPushMpesa/settings.py
+++ stkPushMpesa/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 
 from django.contrib import staticfiles
 
-from stkapp.utility import BASE_NGROK_URL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
